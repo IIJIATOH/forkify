19b916a569828984c680335bc2f256c5e28c8da2
IIJIATOH <platokx@gmail.com>
2023-07-08 21:11:52 +0300

    Git-pages third try

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+// файл vue.config.js расположен в корне вашего репозитория
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/forkify/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/' + process.env.CI_PROJECT_NAME + '/'
+      : '/',
 };
